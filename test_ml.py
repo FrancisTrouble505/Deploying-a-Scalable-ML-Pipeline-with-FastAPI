d1712793c994880df72c5bc2e51bd8b77d3a40f7
Eric McCoy <mccoy1290@gmail.com>
2026-05-06 21:27:05 -0400

    Step 4.2

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,35 @@
 import pytest
 # TODO: add necessary import
+import pandas as pd
 
 # TODO: implement the first test. Change the function name and input as needed
-def test_one():
+def test_data_no_null_value():
     """
-    # add description for the first test
+    # A test that checks that there are no null values.
     """
     # Your code here
+    data = pd.read_csv("data/census.csv")
+    assert data.isnull().sum().sum() == 0
     pass
 
 
 # TODO: implement the second test. Change the function name and input as needed
-def test_two():
+def test_salary_two_values():
     """
-    # add description for the second test
+    # Tests that the salary column only contains two values.
     """
     # Your code here
+    data = pd.read_csv("data/census.csv")
+    assert len(data["salary"].unique()) == 2
     pass
 
 
 # TODO: implement the third test. Change the function name and input as needed
-def test_three():
+def test_data_column_count():
     """
-    # add description for the third test
+    # Tests that the census data contains 15 columns.
     """
     # Your code here
+    data = pd.read_csv("data/census.csv")
+    assert data.shape[1] == 15
     pass
